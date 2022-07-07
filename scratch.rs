32529a1c300f30bfe827357c9b94c1cb15cc23da
Valentin Le Tallec <valentin.le_tallec@hotmail.fr>
2022-07-07 13:09:41 +0200

    remove useless files

--- scratch.rs
+++
@@ -1,4 +0,0 @@
-// .inspect(|line| println!("~~~> {:?}", line))
-// .inspect(|line| println!("DO_UPDATE {:?}", DO_UPDATE.captures(&line)))
-// .inspect(|line| println!("UP_TO_DATE {:?}", UP_TO_DATE.captures(&line)))
-// .inspect(|line| println!("MORE_CHAPTER_THAN_SOURCE {:?}", MORE_CHAPTER_THAN_SOURCE.captures(&line)))
