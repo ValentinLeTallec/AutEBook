cf3289347fb483a394ac0a5b59c73f6dc590a908
Valentin Le Tallec <valentin.le_tallec@hotmail.fr>
2022-08-06 18:35:53 +0200

    Better regexp handling with lazy-regex

--- src/source/royalroad.rs
+++ src/source/royalroad.rs
@@ -1,13 +1,7 @@
 use super::Source;
 use crate::updater::FanFicFare;
 use crate::updater::Update;
-use lazy_static::lazy_static;
-use regex::Regex;
-
-lazy_static! {
-    static ref FICTION_URL_PATTERN: Regex =
-        Regex::new(r"^https://www\.royalroad\.com/fiction/(\d+)(/.{0,100})?$").unwrap();
-}
+use lazy_regex::regex;
 
 #[derive(Debug, PartialEq)]
 pub struct RoyalRoad {
@@ -26,13 +20,16 @@ impl Source for RoyalRoad {
     }
 
     fn new(fiction_url: &str) -> Option<Self> {
-        let captures = FICTION_URL_PATTERN.captures(fiction_url)?;
+        let fiction_url_pattern =
+            regex!(r"^https://www\.royalroad\.com/fiction/(\d+)(/.{0,100})?$");
+        let captures = fiction_url_pattern.captures(fiction_url)?;
         let id = captures[1].parse::<u32>().ok()?;
         Some(Self { id })
     }
 }
 
 #[cfg(test)]
+#[allow(clippy::expect_used)]
 mod tests {
     use super::*;
 
@@ -46,16 +43,27 @@ mod tests {
 
     #[test]
     fn test_royalroad_long_url() {
-        let source =
-            RoyalRoad::new("https://www.royalroad.com/fiction/36049/the-primal-hunter").unwrap();
+        let url = "https://www.royalroad.com/fiction/36049/the-primal-hunter";
         let rss_feed = "https://www.royalroad.com/fiction/syndication/36049";
+        let source = RoyalRoad::new(url).unwrap_or_else(|| {
+            panic!(
+                " The url `{}` could not be recognised as valid for RoyalRoad",
+                url
+            )
+        });
         assert_eq!(source.get_syndication_url(), Some(rss_feed.to_string()));
     }
 
     #[test]
     fn test_royalroad_short_url() {
-        let source = RoyalRoad::new("https://www.royalroad.com/fiction/36049").unwrap();
+        let url = "https://www.royalroad.com/fiction/36049/the-primal-hunter";
         let rss_feed = "https://www.royalroad.com/fiction/syndication/36049";
+        let source = RoyalRoad::new(url).unwrap_or_else(|| {
+            panic!(
+                " The url `{}` could not be recognised as valid for RoyalRoad",
+                url
+            )
+        });
         assert_eq!(source.get_syndication_url(), Some(rss_feed.to_string()));
     }
 }
--- src/updater/fanficfare.rs
+++ src/updater/fanficfare.rs
@@ -1,28 +1,24 @@
 use crate::updater::Update;
 use crate::updater::UpdateResult;
 
-use lazy_static::lazy_static;
-use regex::Regex;
 // use rss::Channel;
+use lazy_regex::regex;
 use std::error::Error;
 use std::io;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 use std::process::{Command, Stdio};
 
-lazy_static! {
-    static ref UPDATING: Regex = Regex::new(r"^Updating .*, URL: .*$").unwrap();
-    static ref UP_TO_DATE: Regex = Regex::new(r"^.* already contains \d+ chapters\.$").unwrap();
-    static ref DO_UPDATE: Regex =
-        Regex::new(r"^Do update - epub\((\d+)\) vs url\((\d+)\)$").unwrap();
-    static ref MORE_CHAPTER_THAN_SOURCE: Regex =
-        Regex::new(r"^.* contains (\d+) chapters, more than source: (\d+)\.$").unwrap();
-}
-
 pub struct FanFicFare;
 
 impl FanFicFare {
     fn do_update(path: &Path) -> Result<UpdateResult, Box<dyn Error>> {
+        let updating = regex!(r"^Updating .*, URL: .*$");
+        let up_to_date = regex!(r"^.* already contains \d+ chapters\.$");
+        let do_update = regex!(r"^Do update - epub\((\d+)\) vs url\((\d+)\)$");
+        let more_chapter_than_source =
+            regex!(r"^.* contains (\d+) chapters, more than source: (\d+)\.$");
+
         let path = path
             .to_str()
             .ok_or_else(|| io::Error::from(io::ErrorKind::Unsupported))?;
@@ -42,17 +38,17 @@ impl FanFicFare {
         let update_result = BufReader::new(stdout)
             .lines()
             .filter_map(std::result::Result::ok)
-            .filter(|line| UPDATING.captures(line).is_none())
+            .filter(|line| updating.captures(line).is_none())
             .find_map(|line| {
-                if UP_TO_DATE.captures(&line).is_some() {
+                if up_to_date.captures(&line).is_some() {
                     return Some(UpdateResult::UpToDate);
                 }
-                if let Some(c) = DO_UPDATE.captures(&line) {
+                if let Some(c) = do_update.captures(&line) {
                     let nb_chapter_epub = &c[1].parse::<u16>().ok()?;
                     let nb_chapter_url = &c[2].parse::<u16>().ok()?;
                     return Some(UpdateResult::Updated(nb_chapter_url - nb_chapter_epub));
                 }
-                if let Some(c) = MORE_CHAPTER_THAN_SOURCE.captures(&line) {
+                if let Some(c) = more_chapter_than_source.captures(&line) {
                     let nb_chapter_epub = &c[1].parse::<u16>().ok()?;
                     let nb_chapter_url = &c[2].parse::<u16>().ok()?;
                     return Some(UpdateResult::MoreChapterThanSource(
