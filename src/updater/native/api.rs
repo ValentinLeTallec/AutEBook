9fe141f19e394c773fea4644860fd20d5a3060e1
Valentin Le Tallec <valentin.le_tallec@hotmail.fr>
2024-11-28 20:40:16 +0100

    refactor: clippy fixes

--- src/updater/native/api.rs
+++ src/updater/native/api.rs
@@ -4,7 +4,7 @@ use color_eyre::eyre;
 pub struct RoyalRoadApi;
 
 impl RoyalRoadApi {
-    pub fn new() -> Self {
+    pub const fn new() -> Self {
         Self
     }
     pub fn get_book(&self, id: u32, ignore_cache: bool) -> eyre::Result<Book> {
@@ -13,48 +13,45 @@ impl RoyalRoadApi {
 
         // Check the cache.
         let cached = Cache::read_book(id)?;
-        match cached {
-            Some(mut cached) => {
-                // Compare cached and fetched to see if any chapters are out-of-date.
-                let mut should_update = ignore_cache;
-                for chapter in book.chapters.iter() {
-                    if let Some(cached) = cached.chapters.iter().find(|c| c.url == chapter.url) {
-                        if cached.date != chapter.date {
-                            should_update = true;
-                            break;
-                        }
-                    } else {
+        if let Some(mut cached) = cached {
+            // Compare cached and fetched to see if any chapters are out-of-date.
+            let mut should_update = ignore_cache;
+            for chapter in book.chapters.iter() {
+                if let Some(cached) = cached.chapters.iter().find(|c| c.url == chapter.url) {
+                    if cached.date != chapter.date {
                         should_update = true;
                         break;
                     }
-                }
-
-                if should_update {
-                    // There is at least one out-of-date chapter, update the chapters.
-                    book.update_chapter_content()?;
-
-                    // Save back to cache.
-                    Cache::write_book(&book)?;
-
-                    Ok(book)
                 } else {
-                    // Just update the cover URL and resave to cache.
-                    cached.cover_url = book.cover_url;
-                    Cache::write_book(&cached)?;
-
-                    Ok(cached)
+                    should_update = true;
+                    break;
                 }
             }
-            None => {
-                // Load book chapters.
+
+            if should_update {
+                // There is at least one out-of-date chapter, update the chapters.
                 book.update_chapter_content()?;
 
-                // Write book to cache.
+                // Save back to cache.
                 Cache::write_book(&book)?;
 
-                // Return book.
                 Ok(book)
+            } else {
+                // Just update the cover URL and resave to cache.
+                cached.cover_url = book.cover_url;
+                Cache::write_book(&cached)?;
+
+                Ok(cached)
             }
+        } else {
+            // Load book chapters.
+            book.update_chapter_content()?;
+
+            // Write book to cache.
+            Cache::write_book(&book)?;
+
+            // Return book.
+            Ok(book)
         }
     }
 }
