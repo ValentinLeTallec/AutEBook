13a2a0001dcb6e04a8153239dd754e06f36c3e16
Valentin Le Tallec <valentin.le_tallec@hotmail.fr>
2025-04-19 12:04:41 +0200

    feat: share bounce between theard

--- src/updater/native/request.rs
+++ src/updater/native/request.rs
@@ -6,29 +6,29 @@ use governor::{DefaultKeyedRateLimiter, Jitter, Quota, RateLimiter};
 use reqwest::blocking::{Client, Response};
 use reqwest::StatusCode;
 use std::num::NonZeroU32;
+use std::sync::atomic::{AtomicU8, Ordering};
 use std::sync::LazyLock;
 use std::thread;
 use std::time::Duration;
 use url::Url;
 
-const USER_AGENT: &str = "AutEBook <https://github.com/ValentinLeTallec/AutEBook>";
-
 pub fn get_text(url: &str) -> Result<String> {
-    send_get_request_rec(url, 4)?
+    send_get_request_rec(url)?
         .error_for_status()
         .and_then(reqwest::blocking::Response::text)
         .map_err(|e| eyre!("Broken link : {e} (URL: {url})"))
 }
 
 pub fn get_bytes(url: &str) -> Result<Bytes> {
-    send_get_request_rec(url, 4)?
+    send_get_request_rec(url)?
         .error_for_status()
         .and_then(reqwest::blocking::Response::bytes)
         .map_err(|e| eyre!("Broken link : {e} (URL: {url})"))
 }
 
-fn send_get_request_rec(url: &str, bounce: u8) -> Result<Response> {
+fn send_get_request_rec(url: &str) -> Result<Response> {
     static CLIENT: LazyLock<Client> = LazyLock::new(Client::new);
+    static BOUNCE: AtomicU8 = AtomicU8::new(0);
     #[allow(clippy::unwrap_used)]
     static RATE_LIMITER: LazyLock<DefaultKeyedRateLimiter<String>> = LazyLock::new(|| {
         RateLimiter::keyed(
@@ -37,8 +37,14 @@ fn send_get_request_rec(url: &str, bounce: u8) -> Result<Response> {
         )
     });
 
-    let host = Url::parse(url)
-        .map_err(|e| eyre!("{e} (URL: {url})"))?
+    let bounce = BOUNCE.load(Ordering::Relaxed);
+    if bounce > 0 {
+        let secs = 8 * 2_u64.pow(bounce.into());
+        MULTI_PROGRESS.eprintln(&eyre!("Too many request, waiting for {secs} s"));
+        thread::sleep(Duration::from_secs(secs));
+    }
+
+    let host = Url::parse(url)?
         .host()
         .map(|h| h.to_string())
         .unwrap_or_default();
@@ -47,19 +53,14 @@ fn send_get_request_rec(url: &str, bounce: u8) -> Result<Response> {
         thread::sleep(Jitter::up_to(Duration::from_millis(30)) + Duration::from_millis(50));
     }
 
-    CLIENT
-        .get(url)
-        .header("User-Agent", USER_AGENT)
-        .send()
-        .map_err(|e| eyre!("{e} (URL: {url})"))
-        .and_then(|e| {
-            if e.status() == StatusCode::TOO_MANY_REQUESTS && bounce <= 10 {
-                let secs = 2_u8.pow(bounce.into()).into();
-                MULTI_PROGRESS.eprintln(&eyre!("Too many request, waiting for {secs} s"));
-                thread::sleep(Duration::from_secs(secs));
-                send_get_request_rec(url, bounce + 1)
-            } else {
-                Ok(e)
-            }
-        })
+    let user_agent = "AutEBook <https://github.com/ValentinLeTallec/AutEBook>";
+    let response = CLIENT.get(url).header("User-Agent", user_agent).send()?;
+
+    if response.status() == StatusCode::TOO_MANY_REQUESTS && bounce <= 10 {
+        BOUNCE.fetch_add(1, Ordering::Relaxed);
+        send_get_request_rec(url)
+    } else {
+        BOUNCE.swap(0, Ordering::Relaxed);
+        Ok(response)
+    }
 }
